2a50e5d86b8021eefe1f4056d843c996919e3520
charlescohanlon <charlescohanlon@gmail.com>
2024-02-21 13:11:44 -0800

    add specific CUDA_VISIBLE_DEVICES

--- src/vllm_server.py
+++ src/vllm_server.py
@@ -1,18 +1,19 @@
-
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,3"
 from vllm import LLM, SamplingParams
 import torch
-#import time
-#import os
+
+# import time
 from constants import MAX_TOKENS
 
 
-#tensor_parallel_size = int(os.environ.get("DEVICES", "1"))
+tensor_parallel_size = len(os.environ.get("CUDA_VISIBLE_DEVICES").split(","))
 
 sampling_params = SamplingParams(max_tokens=MAX_TOKENS)
 
 llm = LLM(
     "mistralai/Mixtral-8x7B-Instruct-v0.1",
-    #tensor_parallel_size=tensor_parallel_size,
-    dtype=torch.bfloat16,
-    gpu_memory_utilization=1.0
+    tensor_parallel_size=tensor_parallel_size,
+    dtype=torch.float16,
+    gpu_memory_utilization=1.0,
 )
